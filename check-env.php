96f0f8d1b04e7f6aaa9822a86edfc4f60bb81fd1
akira1130 <akiralaurenaria30@gmail.com>
2026-08-06 14:08:13 +0900

    Improve environment diagnostics to show all database-related variables

--- check-env.php
+++ check-env.php
@@ -6,6 +6,7 @@
 header('Content-Type: application/json');
 
 $vars = [
+    'DATABASE_URL' => getenv('DATABASE_URL') ? substr(getenv('DATABASE_URL'), 0, 50) . '...' : 'not set',
     'MYSQL_HOST' => getenv('MYSQL_HOST'),
     'MYSQL_USER' => getenv('MYSQL_USER'),
     'MYSQL_PASSWORD' => getenv('MYSQL_PASSWORD') ? '***set***' : 'not set',
@@ -18,10 +19,21 @@ $vars = [
     'DB_PORT' => getenv('DB_PORT'),
 ];
 
+// Show all environment variables for debugging
+$allEnv = getenv();
+$dbRelated = [];
+foreach ($allEnv as $key => $value) {
+    if (stripos($key, 'db') !== false || stripos($key, 'mysql') !== false || stripos($key, 'database') !== false) {
+        $dbRelated[$key] = strlen($value) > 50 ? substr($value, 0, 50) . '...' : $value;
+    }
+}
+
 echo json_encode([
     'environment' => $vars,
+    'database_related_vars' => $dbRelated,
     'php_version' => phpversion(),
     'mysqli_available' => extension_loaded('mysqli') ? 'yes' : 'no',
     'cwd' => getcwd()
 ], JSON_PRETTY_PRINT);
 ?>
+
